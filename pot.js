143e666277b6f52902b3d3b6c900830bb25016cd
MarioBahaa <32718005+Mariobahaa@users.noreply.github.com>
2020-12-21 03:56:42 +0200

    Got a row moving

--- pot.js
+++ pot.js
@@ -1,53 +1,81 @@
 $("#ship").hide();
 $("#lives").hide();
-$("#playbt").mouseenter(function(){$("#playbt").effect("bounce",300);});
+$("#playbt").mouseenter(function () {
+    $("#playbt").effect("bounce", 300);
+});
 
 var lives;
 var score;
 var highScore;
 
 
-function StartGame(){
+function StartGame() {
     PlayAudio("BG");
     ChangeBackgroundImg("seabg.jpg");
     $("#startmenu").hide();
     $("#logo").hide();
     $("#ship").show();
     $("#lives").show();
-    lives=5;
-    score=0;
-    highScore=0; //get highscore using cookies
+    lives = 5;
+    score = 0;
+    highScore = 0; //get highscore using cookies
+    bombId = 0;
     speed = 2000;
     Game();
-    
+
 }
 
-function ChangeBackgroundImg(img){
-    $("body").css("background-image","url("+"./potImgs/"+img+')');
+function ChangeBackgroundImg(img) {
+    $("body").css("background-image", "url(" + "./potImgs/" + img + ')');
 }
-function PlayAudio(audioid){
-    document.querySelector("#"+audioid).play();
+
+function PlayAudio(audioid) {
+    document.querySelector("#" + audioid).play();
 }
 
 var bombTimer;
 var levelTimer;
-var moverTimer;
 var speed;
 var onScreenLetters = "";
-const alphabet = "abcdefghijklmnopqrstuvwxyz"
+const alphabet = "abcdefghijklmnopqrstuvwxyz";
+var bombId; //remove all references
+var moverTimer
 
 
-function Game(){
-    bombTimer = setInterval(function(){
+function Game() {
+
+    bombTimer = setInterval(function () {
         var letter = alphabet[Math.floor(Math.random() * alphabet.length)];
-        onScreenLetters+=letter;
-        var cls = Math.floor(Math.random() * 3)+1;
-        $("body").append("<label class='bomb"+cls+"'>"+letter+"</label>").addClass("cannon"+cls);
-   },speed);
-    levelTimer= setInterval(function(){
-                            if(speed>=200)
-                           speed-=100; 
-                            },20000);
+        onScreenLetters += letter;
+        var cls = Math.floor(Math.random() * 3) + 1;
+        $("body").append("<label class='bomb" + cls + "' id='" + bombId + "'>" + letter + "</label>");
+        bombId++;
+    }, speed);
+    levelTimer = setInterval(function () {
+        if (speed >= 200)
+            speed -= 100;
+    }, 20000);
+    var x = document.querySelectorAll('label')[3];
+    console.log(x);
+    
+    moverTimer = setInterval(function () {
+        var x = document.querySelectorAll(".bomb1");
+        if(x.length!=0){
+            for(var i=0;i<x.length;i++){
+                x[i].style.marginLeft = (parseInt(getComputedStyle(x[i]).marginLeft)-10)+"px";
+            }
+        }
+            
+        },100);
+       /* for (var j = 1; j <= 3; j++)
+            for (var x in document.querySelectorAll(".bomb1")) {
+                //document.querySelectorAll(".bomb" + j)[i].style.marginLeft =
+                    x.style.marginLeft =
+                    //(parseInt(document.querySelectorAll(".bomb" + j)[i].style.marginLeft) - 10) + "px";
+                    (parseInt(x.style.marginLeft) - 10) + "px";
+            }
+    }, 50); // can add a feature here*/
 }
 //fn lose life
 //add listener on key press for body, check if character is in onScreenLetters
+var x = document.querySelectorAll(".bomb1");
