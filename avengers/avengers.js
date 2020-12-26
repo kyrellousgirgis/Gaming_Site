d22c743342dd17170f3c955881182dd809a5020d
MarioBahaa <32718005+Mariobahaa@users.noreply.github.com>
2020-12-26 18:15:30 +0200

    Heroes Fall From Top

--- avengers/avengers.js
+++ avengers/avengers.js
@@ -30,10 +30,51 @@ function PlayAudio(audioid, reset) {
     }
 }
 
-var score;
+             
+var score = 0;
 var playing = false;
+var heroTimer;
+var heroSpeed = 2000;
+var fallTimer;
+var fallSpeed = 5;
 function Game(){
     playing = true;
+    heroTimer= setInterval(function(){
+        var heroId = Math.floor(Math.random()*9)  + 1;
+        var hero = document.querySelector('#h'+heroId);
+        
+        var max = window.innerWidth-hero.width;
+        var pos =  Math.floor(Math.random()*(max));
+        if(pos>=max)
+            pos-=50;
+        else if (pos<=0)
+            pos+=50;
+        
+        if(pos<=50) pos=50;
+        else if(pos>=max-50) pos=max+50;
+        
+        $(hero).css("left",pos +"px");
+        $(hero).css("top",-60);
+        $(hero).show();
+        
+          },heroSpeed);
+        
+        var fallTimer = setInterval(function(){
+            for(var i=0;i<$(".hero").length;i++){
+                var tp =  parseInt($(".hero").eq(i).css("top"));
+                $(".hero").eq(i).css("top",tp+fallSpeed+"px");
+            
+            }
+                
+            
+           for(var i=0;i<$(".villain").length;i++){
+                var tp =  parseInt($(".villain").eq(i).css("top"));
+                $(".villain").eq(i).css("top",tp+fallSpeed+"px");
+            
+            }
+        },25);
+             
+  
 }
 
 
