6233e91f35ff0d981ae64c91630759959bff6321
MarioBahaa <32718005+Mariobahaa@users.noreply.github.com>
2020-12-26 04:09:39 +0200

    Added Home Page

---
+++ avengers/avengers.js
@@ -0,0 +1,12 @@
+$("body").children().hide();
+
+
+$("#htp").fadeIn(500);
+
+$("#start").mouseover(function(){
+    $(this).css("opacity","100%");
+})
+
+$("#start").mouseout(function(){
+    $(this).css("opacity","80%");
+})
